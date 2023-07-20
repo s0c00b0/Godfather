79f222da2447ba54847a34afa7835300f895bc2c
Bo Sun <socoobo@seas.upenn.edu>
2023-07-20 08:56:03 -0400

    preprocessor progress

--- microread-model/preprocess.py
+++ microread-model/preprocess.py
@@ -1,4 +1,41 @@
 '''Preprocess posts from Mafia Universe games into model input.'''
 
-def process_post(post):
+import re
+import string
+
+def remove_quotes(post):
+    # remove outer quote tags
+    for i in range(0, len(post)):
+        if post[i] == ']':
+            post = post[i+1:]
+            break
+    for i in range(1, len(post)):
+        if post[-1 * i] == '[':
+            post = post[:-1 * i - 1]
+            break
+            
+    # search through string for quotes and remove
+    post = re.sub('\[QUOTE=.*?\].*\[/QUOTE\]', '', post)
     
+    return post
+
+def trim(post):
+    # TODO
+    pass
+    
+def preprocess_post(post):
+    # replace all whitespace characters with spaces
+    post = re.sub('\s', ' ', post)
+    
+    # remove all quotes as well as outer quote tags
+    post = remove_quotes(post)
+    
+    # remove all BBCode, emojis, and images
+    post = trim(post)
+    
+    # remove all writing style differences (i.e. upper/lowercase, punctuation, etc.)
+    post = post.lower()
+    post = post.translate(post.maketrans('', '', string.punctuation))
+    post = post.strip()
+    
+    return post
--- microread-model/train.py
+++ microread-model/train.py
@@ -7,7 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss, accuracy_score
 from xgboost import XGBClassifier
 
-def generate_model(text_array, label_array):
+def generate_model(text_array, label_array,
+                    n_estimators = 1000, learning_rate = 0.05, early_stopping_rounds = 10):
     nlp = spacy.load('en_core_web_lg')
     embed = np.array([nlp(text).vector for text in text_array])
     X_train, X_test, y_train, y_test = train_test_split(embed, label_array)
